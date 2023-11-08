d01dd3e487d81eb5c54c4dd8fe8055b40cf1b8b4
Fazil Shafi <fazil_bhat@softpodigy.com>
2023-11-08 18:55:10 +0530

    assignment 2

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+interface User {
+  serial_no: number;
+  name: string;
+  email: string;
+}
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class Assignment2 {
+  
+  users: User[] = [
+    { serial_no: 1, name: 'Fazil', email: 'fazil123@gmail.com'},
+    { serial_no: 2, name: 'Nashra', email: 'nashra123@gmail.com'},
+    { serial_no: 3, name: 'Amit', email: 'amit123@gmail.com'}
+  ]
+}
