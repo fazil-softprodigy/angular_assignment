b6d219b394234e53b7f1de4ab34bcadcdfdf2959
Fazil Shafi <fazil_bhat@softpodigy.com>
2023-11-30 14:19:41 +0530

    week4 assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Assignment2 } from './user/user.component';
 import { HomeComponent } from './home/home.component';
 import { UserModule } from './user/user.module';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Week4assignmentComponent } from './week4assignment/week4assignment.component';
+import { HttpInterceptors } from './services/http.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
     // Assignment2,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    Week4assignmentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     UserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpInterceptors, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
